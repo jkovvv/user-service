c87102d9d497989acf6d830bf51e57016facc8ef
Janko <jankokovacevicc@gmail.com>
2026-07-14 14:34:38 +0200

    Initial User Service setup

---
+++ src/user/Main.java
@@ -0,0 +1,14 @@
+package user;
+
+public class Main {
+    package user;
+
+    public class Main {
+
+        public static void main(String[] args) {
+
+            System.out.println("User Service starting...");
+
+        }
+    }
+}
